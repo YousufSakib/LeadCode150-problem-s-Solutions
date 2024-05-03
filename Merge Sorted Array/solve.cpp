01f395ce46c100ca75fbfc1ecdabb1af9a377eb7
yousufSakib <yousufsakib29603@gmail.com>
2024-05-03 19:48:13 +0600

    Added solve.cpp to the problem 'Merge Sorted Array'

---
+++ Merge Sorted Array/solve.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        sort(nums1.begin(), nums1.begin() + m);
+        sort(nums2.begin(), nums2.end());
+        vector<int> temp(m);
+        for (int i = 0; i < m; i++)
+            temp[i] = nums1[i];
+
+        int tempIndx = 0, nums2Indx = 0, nums1Indx = 0;
+        while (true)
+        {
+
+            if (tempIndx == m)
+            {
+                while (nums2Indx < n)
+                {
+                    nums1[nums1Indx] = nums2[nums2Indx];
+                    nums1Indx++;
+                    nums2Indx++;
+                }
+                break;
+            }
+            else if (nums2Indx == n)
+            {
+                while (tempIndx < m)
+                {
+                    nums1[nums1Indx] = temp[tempIndx];
+                    nums1Indx++;
+                    tempIndx++;
+                }
+                break;
+            }
+            else if (temp[tempIndx] < nums2[nums2Indx])
+            {
+                nums1[nums1Indx] = temp[tempIndx];
+                nums1Indx++;
+                tempIndx++;
+            }
+            else
+            {
+                nums1[nums1Indx] = nums2[nums2Indx];
+                nums2Indx++;
+                nums1Indx++;
+            }
+        }
+    }
+};
---
+++ README.txt
@@ -0,0 +1,32 @@
+# Leetcode's 150 top interview problem's Solutions
+
+This repository contains solutions to the top interview problems from LeetCode. Each problem is solved using JavaScript and C++.
+
+
+## Introduction
+
+LeetCode is a popular platform for practicing coding skills and preparing for technical interviews. This repository aims to provide solutions to the top 150 interview problems from LeetCode.
+
+## Problem List
+
+Here is a list of the problems included in this repository:
+
+1. Problem 1
+    - Language: JS, C++
+    - Tag: Array / String
+    - problemLink: https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+2. Problem 2
+    - Language: JS, C++
+    - Tag: Array / String
+    - problemLink: 
+
+...
+
+## Contributing
+
+Contributions are welcome! If you have a solution to a problem that is not already included in this repository, feel free to open a pull request. Please make sure to follow the contribution guidelines outlined in the [CONTRIBUTING.md](./CONTRIBUTING.md) file.
+
+## License
+
+This project is licensed under the [MIT License](./LICENSE).    
