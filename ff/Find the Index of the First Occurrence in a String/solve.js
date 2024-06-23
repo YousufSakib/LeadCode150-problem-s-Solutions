7e4dc5d941e63326f8b846b36e411423ef4813c8
YousufSakib <119650560+YousufSakib@users.noreply.github.com>
2024-06-23 16:36:33 +0600

    Delete ff directory

--- ff/Find the Index of the First Occurrence in a String/solve.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param {string} haystack
- * @param {string} needle
- * @return {number}
- */
-var strStr = function (a, b) {
-    return a.indexOf(b);
-};
--- ff/Reverse Words in a String/solve.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {string} s
- * @return {string}
- */
-var reverseWords = function (s) {
-    return s.trim().replace(/\s+/g, " ").split(" ").reverse().join(" ");
-};
